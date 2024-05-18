42de9c584a10a54281330bfe00f9dca0866e274b
Stanislava <stasya@guseva.spb.ru>
2024-05-18 16:06:40 +0300

    All dependencies are set using .xml file.

--- Bot/src/main/java/org/example/TestSpring.java
+++ Bot/src/main/java/org/example/TestSpring.java
@@ -7,11 +7,12 @@ public class TestSpring {
         ClassPathXmlApplicationContext contex = new ClassPathXmlApplicationContext("applicationContext.xml");
         //in .xml we can change RockMusick->ClassicalMusin. And don't change code.
 
-    Music music = contex.getBean("musicBean", Music.class);
+    //Music music = contex.getBean("musicBean", Music.class);
+    //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-    MusicPlayer musicPlayer = new MusicPlayer(music);
-    musicPlayer.PlayMusic();
+        MusicPlayer musicPlayer = contex.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.PlayMusic();
 
-    contex.close();
+        contex.close();
     }
 }
