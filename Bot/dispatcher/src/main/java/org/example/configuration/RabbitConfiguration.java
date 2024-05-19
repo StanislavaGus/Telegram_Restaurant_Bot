770ca2ae0077f4dee53525ed117b5cc884300ef6
marg0sav <margo@savenko.su>
2024-05-19 17:11:11 +0300

    Configuring RabbitMQ.

--- Bot/dispatcher/src/main/java/org/example/configuration/RabbitConfiguration.java
+++ Bot/dispatcher/src/main/java/org/example/configuration/RabbitConfiguration.java
@@ -1,6 +1,9 @@
 package org.example.configuration;
 
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -11,21 +14,29 @@ import static org.example.model.RabbbitQueue.*;
 
 
 @Configuration
+@Log4j2
 public class RabbitConfiguration {
     @Bean
     public MessageConverter jsonMessageConvertor(){
         return new Jackson2JsonMessageConverter();
     }
     @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+    @Bean
     public Queue textMessageQueue(){
+        log.info("Creating text_message_update queue");
         return new Queue(TEXT_MESSAGE_UPDATE);
     }
     @Bean
     public Queue docMessageQueue(){
+        log.info("Creating doc_message_update queue");
         return new Queue(DOC_MESSAGE_UPDATE);
     }
     @Bean
     public Queue photoMessageQueue(){
+        log.info("Creating photo_message_update queue");
         return new Queue(PHOTO_MESSAGE_UPDATE);
     }
     @Bean
